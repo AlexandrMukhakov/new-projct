daa0a0fcf4d148a8901741c8726b6e764c834c95
alexandr <xavi08@yandex.ru>
2022-01-05 21:00:45 +0300

    function mod2

--- script.js
+++ script.js
@@ -19,17 +19,23 @@ const personalMoviesDB = {
     priat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Последний посмотренный фильм",""),
-        b = prompt("Оцените его","");
-
-if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalMoviesDB.movies[a] = b;
-    console.log("done");
-} else {
-    console.log("Error");
-    i--;}
+function PersonalLevel() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt("Последний посмотренный фильм",""),
+            b = prompt("Оцените его","");
+    
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMoviesDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("Error");
+        i--;}
 }
+}
+PersonalLevel();
+
+function detectPersonalLevel() {
+
 if (personalMoviesDB.count < 10) {
     console.log("Просмотренно слишком мало фильмов","");
 } else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
@@ -39,4 +45,19 @@ if (personalMoviesDB.count < 10) {
 } else {
     console.log("Произошла ошибка","");
 }
-    console.log(personalMoviesDB); 
+}
+detectPersonalLevel();
+
+function ShowMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMoviesDB);
+    }
+}
+ShowMyDB(personalMoviesDB.priat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+    personalMoviesDB.genres[i - 1] = prompt(`Ваш любымый жанр? ${i}`);
+}
+}
+writeYourGenres();
